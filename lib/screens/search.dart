d7bf81262a6d8ea9388dd4e5566178a184195241
kaalaNAAG <gbhargav041@gmail.com>
2021-10-10 05:09:16 +0530

    added distance calculator

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -12,25 +12,63 @@ class Search extends StatelessWidget {
   Widget build(BuildContext context) {
     final currentPosition = Provider.of<Position>(context);
     final placesProvider = Provider.of<Future<List<Place>>>(context);
+    final geoService = GeoLocatorService();
     return FutureProvider(
       initialData: null,
       create: (context) => placesProvider,
       child: Scaffold(
         body: (currentPosition != null)
-            ? Column(
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 3,
-                    width: MediaQuery.of(context).size.width,
-                    child: GoogleMap(
-                      initialCameraPosition: CameraPosition(
-                          target: LatLng(currentPosition.latitude,
-                              currentPosition.longitude),
-                          zoom: 16.0),
-                      zoomGesturesEnabled: true,
-                    ),
-                  ),
-                ],
+            ? Consumer<List<Place>>(
+                builder: (_, places, __) {
+                  return (places != null)
+                      ? Column(
+                          children: <Widget>[
+                            Container(
+                              height: MediaQuery.of(context).size.height / 3,
+                              width: MediaQuery.of(context).size.width,
+                              child: GoogleMap(
+                                initialCameraPosition: CameraPosition(
+                                    target: LatLng(currentPosition.latitude,
+                                        currentPosition.longitude),
+                                    zoom: 16.0),
+                                zoomGesturesEnabled: true,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16.0,
+                            ),
+                            Expanded(
+                              child: ListView.builder(
+                                  itemCount: places.length,
+                                  itemBuilder: (context, index) {
+                                    return FutureProvider(
+                                      initialData: null,
+                                      create: (context) =>
+                                          geoService.getDistance(
+                                              currentPosition.latitude,
+                                              currentPosition.longitude,
+                                              places[index]
+                                                  .geometry
+                                                  .location
+                                                  .lat,
+                                              places[index]
+                                                  .geometry
+                                                  .location
+                                                  .lng),
+                                      child: Card(
+                                        child: ListTile(
+                                          title: Text(places[index].name),
+                                        ),
+                                      ),
+                                    );
+                                  }),
+                            )
+                          ],
+                        )
+                      : Center(
+                          child: CircularProgressIndicator(),
+                        );
+                },
               )
             : Center(
                 child: CircularProgressIndicator(),
--- lib/services/places_service.dart
+++ lib/services/places_service.dart
@@ -7,7 +7,7 @@ class PlacesService {
 
   Future<List<Place>> getPlaces(double lat, double lng) async {
     var response = await http.get(Uri.parse(
-        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&type=parking&rankby=distance&key=$key'));
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&type=hospital&rankby=distance&key=$key'));
     var json = convert.jsonDecode(response.body);
     var jsonResults = json['results'] as List;
     return jsonResults.map((place) => Place.fromJson(place)).toList();
